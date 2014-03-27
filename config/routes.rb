7f1cd5b12d2b4991f8567a698c649cd54d19158f
Zane Wolfgang Pickett <Zane.Wolfgang.Pickett@Gmail.com>
2014-03-27 18:39:57 +0000

    setup route and migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Workspace::Application.routes.draw do
-  resources :shops
+  resources :shops, path: '/'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
